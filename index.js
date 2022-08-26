dc56384be2cfdc64f25d2766a07c36e3b2bce6c8
Digvijay Singh <digvijay29081999@gmail.com>
2022-08-26 18:09:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,125 @@
+/* SHOW MENU */
+    const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+    /* MENU SHOW */
+        if(navToggle){
+            navToggle.addEventListener('click', () =>{
+                navMenu.classList.add('show-menu')
+            })
+        }
+
+    /* MENU HIDDEN */
+        if(navClose){
+            navClose.addEventListener('click', () =>{
+                navMenu.classList.remove('show-menu')
+            })
+        }
+
+    /* REMOVE MENU MOBILE */
+        const navLink = document.querySelectorAll('.nav_link')
+
+        const linkAction = () =>{
+        const navMenu = document.getElementById('nav-menu')
+        navMenu.classList.remove('show-menu')
+        }
+
+        navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/* CHANGE BACKGROUND HEADER */
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/* POPULAR SWIPER */
+let swiperPopular = new Swiper(".popular_container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          spaceBetween: 48,
+        },
+    }
+  });
+
+/* MIXITUP FILTER FEATURED */
+let mixerFeatured = mixitup('.featured_content', {
+    selectors: {
+        target: '.featured_card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/* Link active featured */ 
+const linkFeatured = document.querySelectorAll('.featured_item')
+
+function activeFeatured(){
+    linkFeatured.forEach(l => l.classList.remove('active-featured'))
+    this.classList.add('active-featured')
+}
+
+linkFeatured.forEach(l => l.addEventListener('click', activeFeatured))
+
+/* SHOW SCROLL UP */ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/* SCROLL REVEAL ANIMATION */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.home_title, .popular_container, .features_img, .featured_filters')
+sr.reveal('.home_subtitle', {delay: 500})
+sr.reveal('.home_elec', {delay: 600})
+sr.reveal('.home_img', {delay: 800})
+sr.reveal('.home_car-data', {delay: 900, interval: 100, origin: 'bottom'})
+sr.reveal('.home_button', {delay: 1000, origin: 'bottom'})
+sr.reveal('.about_group, .offer_data', {origin: 'left'})
+sr.reveal('.about_data, .offer_img', {origin: 'right'})
+sr.reveal('.features_map', {delay: 600, origin: 'bottom'})
+sr.reveal('.features_card', {interval: 300})
+sr.reveal('.featured_card, .logo_content, .footer_content', {interval: 100})
